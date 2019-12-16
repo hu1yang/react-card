addfaf0c1a8ada2075695a070935fc6531bdfd1c
hu1yang <ainixiaopihai@icloud.com>
2019-12-16 20:54:31 +0800

    hj

--- src/store/reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux-immutable'
-import { reducer as cardReducer } from '../pages/card/store'
-
-
-const reducer = combineReducers({
-    card: cardReducer,
-})
-export default reducer
